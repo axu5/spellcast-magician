41b22648393c44ebf85cb88e803da09c2a741fb8
Axu <321axu123@gmail.com>
2023-07-10 03:22:46 +0200

    check array

--- src/helpers/readyToSolve.ts
+++ src/helpers/readyToSolve.ts
@@ -2,8 +2,13 @@ import isLetter from "./isLetter";
 
 export default function readyToSolve(grid: string[][]) {
     for (let i = 0; i < grid.length; ++i) {
-        for (let j = 0; j < grid[i].length; ++j) {
-            if (!isLetter(grid[i][j])) {
+        const row = grid[i];
+        if (!row) {
+            return false;
+        }
+        for (let j = 0; j < row.length; ++j) {
+            const letter = row[j];
+            if (!letter || !isLetter(letter)) {
                 return false;
             }
         }
