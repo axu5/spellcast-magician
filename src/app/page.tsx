fbd94bfbf09fe7e9451ae8423b8c5a0be133171a
Axu <321axu123@gmail.com>
2023-07-10 03:28:11 +0200

    better clarification of usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center p-24'>
             <h1 className='text-3xl'>Spellcast Magician</h1>
-            <h2 className='text-xl:'>
-                Enter your board here (Right click to add attributes)
+            <h2 className='text-zinc-800'>
+                Enter your board here (Right click to add attributes
+                and CAPS for gems)
             </h2>
             <SpellcastInput />
         </main>
